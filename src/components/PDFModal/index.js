a59deb412825e174740ff3afab4f2b812b57d92d
José Bezerra <jbmn2@cin.ufpe.br>
2020-09-25 09:08:52 -0300

    fix(PDFModal): fixed the close button at PDFModal

--- src/components/PDFModal/index.js
+++ src/components/PDFModal/index.js
@@ -16,7 +16,7 @@ const PDFModal = ({ PDFFileSrc, closeModal }) => {
                 />
             </div>
             <Document file={PDFFileSrc}>
-                <Page pageNumber={1} width={500} />
+                <Page pageNumber={1} width={480} />
             </Document>
         </div>
     );
