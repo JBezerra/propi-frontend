03b4574d324d4683492ab62d3d77eb20063b9b19
José Bezerra <jbmn2@cin.ufpe.br>
2020-10-30 13:15:48 -0300

    refactor(dependencies): react dependencies cleaned

--- src/pages/ConsultList/index.js
+++ src/pages/ConsultList/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactGA from 'react-ga';
 
 import { useHistory, useLocation } from 'react-router-dom'
